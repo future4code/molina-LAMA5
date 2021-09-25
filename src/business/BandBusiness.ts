f3396470601b24618e8cb8dd4e852a1d9dd592f9
izabellacbrandao <bellacbrandao@gmail.com>
2021-09-24 22:18:39 -0300

    :package: adicionando banda

---
+++ src/business/BandBusiness.ts
@@ -0,0 +1,34 @@
+import { BandDataBase } from "../data/BandDataBase";
+import { InvalidInputError } from "../error/InvalidInputError";
+import { UnauthorizedError } from "../error/UnauthorizedError";
+import { Band, BandInputDTO } from "../model/Band";
+import { UserInputDTO, UserRole } from "../model/User";
+import { Authenticator } from "../services/Authenticator";
+import { IdGenerator } from "../services/IdGenerator";
+
+export class BandBusiness {
+    constructor(
+        private bandDataBase: BandDataBase,
+        private idGenerator: IdGenerator,
+        private authenticator: Authenticator
+    ){}
+
+    async registerBand(input: BandInputDTO, token: string){
+        const tokenData = this.authenticator.getData(token)
+
+        if(tokenData.role !== UserRole.ADMIN){
+            throw new UnauthorizedError("Acesso autorizado apenas para administrador")
+        }
+
+        if(!input.name || !input.mainGenre || !input.responsible){
+            throw new InvalidInputError("Todos os campos devem ser preenchidos")
+        }
+
+        await this.bandDataBase.createBand(
+            Band.toBand({
+                ...input,
+                id: this.idGenerator.generate()
+            })!
+        )
+    }
+}
---
+++ src/data/BandDataBase.ts
@@ -0,0 +1,22 @@
+import { Band } from "../model/Band";
+import { BaseDatabase } from "./BaseDatabase";
+
+export class BandDataBase extends BaseDatabase{
+    private static TABLE_NAME = "lama_band"
+
+    public async createBand(band: Band): Promise<void> {
+        try{
+            await this.getConnection()
+            .insert({
+                id: band.getId(),
+                name: band.getName(),
+                musical_genre: band.getMainGenre(),
+                resposible: band.getResponsible()
+            })
+            .into(BandDataBase.TABLE_NAME)
+
+        }catch(error: any){
+            throw new Error(error.sqlMessage || error.message)
+        }
+    }
+}
